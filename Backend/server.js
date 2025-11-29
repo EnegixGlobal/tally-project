eaafcaf8531397ed1a029e914a11e50165bf3c64
Mustak Ansary <mustak65ee@gmail.com>
2025-11-29 10:18:33 +0530

    changes in backend cors

--- Backend/server.js
+++ Backend/server.js
@@ -6,16 +6,19 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
-// app.use(cors({
-//   origin: [
-//     "http://localhost:5174",
-//     "http://31.97.202.210",
-//     "https://apnabook.com",  // If you host domain later
-//   ],
-//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//   credentials: true
-// }));
+// app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://api.apnabook.com",
+      "http://localhost:5173",
+      "http://localhost:5174",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: false,
+  })
+);
 
 app.use(bodyParser.json());
 // const authMiddleware = require('./middlewares/authMiddleware');
