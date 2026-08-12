8acc9dbe02cc688ad21f4997473a5396566ff2ae
Ayush Kumar <ayushsingh0455@gmail.com>
2026-08-12 11:24:17 +0530

    b2c update

--- frontend/src/components/reports/B2C.tsx
+++ frontend/src/components/reports/B2C.tsx
@@ -94,8 +94,8 @@ const B2C: React.FC = () => {
 
   const [showFilterPanel, setShowFilterPanel] = useState(false);
   const [filters, setFilters] = useState<FilterState>({
-    dateRange: 'this-month',
-    fromDate: new Date(new Date().getFullYear(), new Date().getMonth(), 1).toISOString().split('T')[0],
+    dateRange: 'this-year',
+    fromDate: new Date(new Date().getFullYear(), 0, 1).toISOString().split('T')[0],
     toDate: new Date().toISOString().split('T')[0],
     customerFilter: '',
     paymentMethod: '',
